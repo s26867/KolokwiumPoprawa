c339b37d5418992b4fabcf78fa90b2b07bc8bd1c
s26867 <s26867@pjwstk.edu.pl>
2025-06-22 10:59:57 +0200

    Wzor

---
+++ KolokwiumPoprawa/Data/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KolokwiumPoprawa.Data;
+
+public class MyDbContext : DbContext
+{
+    //TAvele tutaj
+    
+    public MyDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    //Tutaj polaczenie jesli PKFK 
+    
+}
