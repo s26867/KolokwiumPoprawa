0868610efa483743983561d0645e1a4766dea362
s26867 <s26867@pjwstk.edu.pl>
2025-06-22 10:59:12 +0200

    Wzor

---
+++ KolokwiumPoprawa/Services/DbService.cs
@@ -0,0 +1,15 @@
+﻿using KolokwiumPoprawa.Data;
+
+namespace KolokwiumPoprawa.Services;
+
+public interface IDbService
+{
+    //Taski
+}
+
+public class DbService(MyDbContext data) : IDbService
+{
+    //Metody get i post
+    
+    
+}
