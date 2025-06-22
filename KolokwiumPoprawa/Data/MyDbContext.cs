cb1f64f7a8082e689d5f8194443919495ac5788b
s26867 <s26867@pjwstk.edu.pl>
2025-06-22 20:35:27 +0200

    zwrot

--- KolokwiumPoprawa/Data/MyDbContext.cs
+++ KolokwiumPoprawa/Data/MyDbContext.cs
@@ -1,15 +1,41 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using KolokwiumPoprawa.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace KolokwiumPoprawa.Data;
 
 public class MyDbContext : DbContext
 {
-    //TAvele tutaj
+    public DbSet<Book> Books { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserBookFavorite> UserBookFavorites { get; set; }
+    public DbSet<Loan> Loans { get; set; }
     
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserBookFavorite>()
+            .HasKey(e => new { e.Book_ID, e.User_ID });
+        modelBuilder.Entity<UserBookFavorite>()
+            .HasOne(e => e.Book)
+            .WithMany(s => s.UserBookFavorites)
+            .HasForeignKey(e => e.Book_ID);
+        modelBuilder.Entity<UserBookFavorite>()
+            .HasOne(e => e.User)
+            .WithMany(c => c.UserBookFavorites)
+            .HasForeignKey(e => e.User_ID);
+        modelBuilder.Entity<Loan>()
+            .HasKey(e => new { e.Book_ID, e.User_ID });
+        modelBuilder.Entity<Loan>()
+            .HasOne(e => e.Book)
+            .WithMany(s => s.Loans)
+            .HasForeignKey(e => e.Book_ID);
+        modelBuilder.Entity<Loan>()
+            .HasOne(e => e.User)
+            .WithMany(c => c.Loans)
+            .HasForeignKey(e => e.User_ID);
+            
+            
+    }
     public MyDbContext(DbContextOptions options) : base(options)
     {
     }
-    
-    //Tutaj polaczenie jesli PKFK 
-    
 }
--- KolokwiumPoprawa/Services/DbService.cs
+++ KolokwiumPoprawa/Services/DbService.cs
@@ -1,15 +1,37 @@
 ﻿using KolokwiumPoprawa.Data;
+using KolokwiumPoprawa.DTOs;
+using KolokwiumPoprawa.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace KolokwiumPoprawa.Services;
 
 public interface IDbService
 {
-    //Taski
+    Task<IEnumerable<GetUsersDetails>> GetUsers(int id);
+    //Task<IEnumerable<PostUser>> PostUsers(int id);
+    
 }
 
 public class DbService(MyDbContext data) : IDbService
 {
-    //Metody get i post
+    public async Task<IEnumerable<GetUsersDetails>> GetUsers(int id )
+    {
+        return await data.Users.Select(p => new GetUsersDetails
+        {
+            ID = id,
+            Name = p.Name,
+            Email = p.Email,
+            Books = p.UserBookFavorites.Select(ee => new BookDto
+            {
+                ID = ee.Book.ID,
+                Title = ee.Book.Title,
+                Author = ee.Book.Author
+            }).ToList()
+        }).ToListAsync();
+    }
+
+   
+    
     
     
 }
